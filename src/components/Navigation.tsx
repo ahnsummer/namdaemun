f94d251a1cf2753d7deb77f11e1282c21baa97c4
ahnsummer <bazaar9095@gmail.com>
2024-01-28 16:27:04 +0900

    init

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,26 @@
+import { FlexCenter, StlyedDiv } from "@/styles/globals";
+import { Button } from "@mui/base";
+import { TextField } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navigation = () => {
+
+  return (
+    <StlyedDiv className="p-2 border-b">
+      <Link href={'/'}>
+        <Image src="/images/namdaemun.svg" width={100} height={100} alt="logo" priority />
+      </Link>
+      <TextField label="search" className="w-80" />
+      <Link className="flex" href={`tel:757-0095`}>
+        <Image src="/images/telephone.png" width={20} height={20} alt="telephone" priority />
+        <span>757-0095</span>
+      </Link>
+      <Button onClick={() => alert("장바구니가 클릭됐습니다")}>
+        <Image src="/images/shopping-cart.png" width={20} height={30} alt="shopping cart" />
+      </Button>
+    </StlyedDiv>
+  )
+}
+
+export default Navigation;
